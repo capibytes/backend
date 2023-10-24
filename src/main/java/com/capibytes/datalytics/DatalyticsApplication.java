e957ab3789f7df6a50ecb61facc81c16e4ec8241
Júnior <clovispilgerjunior@hotmail.com>
2023-10-24 19:25:31 -0300

    :tada: projeto iniciado

---
+++ src/main/java/com/capibytes/datalytics/DatalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.capibytes.datalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatalyticsApplication.class, args);
+	}
+
+}
