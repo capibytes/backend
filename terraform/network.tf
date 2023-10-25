c7cb7730317b341045f577a966c127ce5ea1f915
Cecília Gonçalves <ceciliavgonc12@gmail.com>
2023-10-24 21:19:24 -0300

    configurando endereço interno

--- terraform/network.tf
+++ terraform/network.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "cpb_subnet1" {
 
 resource "aws_subnet" "cpb_subnet2" {
   vpc_id     = aws_vpc.cpb_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "capibytes_subnet2"
