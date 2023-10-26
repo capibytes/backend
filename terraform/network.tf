fcd6e9146910e0fd136f139e1f36c1deefba0387
Cecília Gonçalves <ceciliavgonc12@gmail.com>
2023-10-25 22:01:25 -0300

    ipv4 pub

--- terraform/network.tf
+++ terraform/network.tf
@@ -11,6 +11,7 @@ resource "aws_subnet" "cpb_subnet1" {
   vpc_id     = aws_vpc.cpb_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "capibytes_subnet1"
   }
@@ -20,6 +21,7 @@ resource "aws_subnet" "cpb_subnet2" {
   vpc_id     = aws_vpc.cpb_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
   tags = {
     Name = "capibytes_subnet2"
   }
--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,8 @@ output "rds_hostname" {
   description = "The hostname of the RDS instance"
   value       = aws_db_instance.cpb_rds.endpoint
 }
+
+output "ec2_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.cpb_ec2.public_dns
+}
