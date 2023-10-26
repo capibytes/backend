4f1a789f19ea9b628ee5d1cd206f85876e3b9185
jonasesoft <jonasesof@gmail.com>
2023-10-26 19:25:59 -0300

    renamed repository class for entity

--- src/main/java/com/capibytes/datalytics/repositories/ConsultantRepository.java
+++ src/main/java/com/capibytes/datalytics/repositories/ConsultantRepository.java
@@ -1,8 +1,9 @@
 package com.capibytes.datalytics.repositories;
 
+import com.capibytes.datalytics.domain.entities.Consultant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConsultantRepository extends JpaRepository <ConsultantRepository, Long>{
+public interface ConsultantRepository extends JpaRepository<Consultant, Long> {
 }
--- src/main/java/com/capibytes/datalytics/repositories/ContactRepository.java
+++ src/main/java/com/capibytes/datalytics/repositories/ContactRepository.java
@@ -1,8 +1,9 @@
 package com.capibytes.datalytics.repositories;
 
+import com.capibytes.datalytics.domain.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContactRepository extends JpaRepository <ContactRepository, Long>{
+public interface ContactRepository extends JpaRepository <Contact, Long>{
 }
