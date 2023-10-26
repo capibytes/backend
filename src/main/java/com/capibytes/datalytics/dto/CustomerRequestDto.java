4dbc599d7d3747bc0cc9a63607d192717c2da784
Daniel Rodrigo Beltrame <danielrbeltrame@gmail.com>
2023-10-26 11:54:55 -0300

    Criando Dto de customer

---
+++ src/main/java/com/capibytes/datalytics/dto/CustomerRequestDto.java
@@ -0,0 +1,13 @@
+package com.capibytes.datalytics.dto;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+public class CustomerRequestDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
