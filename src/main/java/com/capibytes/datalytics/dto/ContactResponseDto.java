29160b73b0512ca68d76a3d872e58cc7db5bc499
Daniel Rodrigo Beltrame <danielrbeltrame@gmail.com>
2023-10-26 12:06:05 -0300

    Criando Dto de Contanct

---
+++ src/main/java/com/capibytes/datalytics/dto/ContactResponseDto.java
@@ -0,0 +1,13 @@
+package com.capibytes.datalytics.dto;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+public class ContactResponseDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
