f01b8cb23e673fac6ff845fa5388d9a447ce46f0
jonasesoft <jonasesof@gmail.com>
2023-10-26 19:19:34 -0300

    renamed repository class

--- src/main/java/com/capibytes/datalytics/repositories/ConsultantRepository.java
+++ src/main/java/com/capibytes/datalytics/repositories/ConsultantRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Customer extends JpaRepository <Customer, Long>{
+public interface ConsultantRepository extends JpaRepository <ConsultantRepository, Long>{
 }
