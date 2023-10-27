8825af0929e99b7a06b1b0fc73baf43594a8fcce
Júnior <clovispilgerjunior@hotmail.com>
2023-10-26 21:01:25 -0300

    :recycle: refactor/entities: alajustado a anotaação da service

---
+++ src/main/java/com/capibytes/datalytics/domain/mappers/MapStructMapper.java
@@ -0,0 +1,49 @@
+package com.capibytes.datalytics.domain.mappers;
+
+import com.capibytes.datalytics.domain.entities.Consultant;
+import com.capibytes.datalytics.domain.entities.Contact;
+import com.capibytes.datalytics.domain.entities.Customer;
+import com.capibytes.datalytics.domain.entities.Form;
+import com.capibytes.datalytics.dtos.request.ConsultantRequestDTO;
+import com.capibytes.datalytics.dtos.request.ContactRequestDTO;
+import com.capibytes.datalytics.dtos.request.CustomerRequestDTO;
+import com.capibytes.datalytics.dtos.request.FormRequestDTO;
+import com.capibytes.datalytics.dtos.response.ContactResponseDTO;
+import com.capibytes.datalytics.dtos.response.CustomerResponseDTO;
+import com.capibytes.datalytics.dtos.response.FormResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "Spring")
+public interface MapStructMapper {
+
+    // Instancia para utlizar o MapStruct caso queira
+    MapStructMapper INSTANCE = Mappers.getMapper(MapStructMapper.class);
+
+
+    List<ConsultantRequestDTO> consultantRequestDTOAllToConsultant(Consultant consultant);
+
+    Consultant consultantToConsultantResponseDTO(ContactResponseDTO contactResponseDTO);
+
+    ConsultantRequestDTO consultantRequestDTOToConsultant(Consultant consultant);
+
+    List<CustomerRequestDTO> customerRequestDTOAllToCustomer(Customer customer);
+
+    Customer customerToCustomerResponseDTO(CustomerResponseDTO customerResponseDTO);
+
+    CustomerRequestDTO customerRequestDTOToCustomer(Customer customer);
+
+
+    List<ContactRequestDTO> contactRequestDTOAllToContact(Contact contact);
+
+    Contact contactToContactResponseDTO(ContactResponseDTO contactResponseDTO);
+
+    ContactRequestDTO contactRequestDTOToContact(Contact contact);
+
+    Form formToFormResponseDTO(FormResponseDTO formResponseDTO);
+
+    FormRequestDTO formRequestDTOToForm(Form form);
+
+}
--- src/main/java/com/capibytes/datalytics/domain/services/ConsultantService.java
+++ src/main/java/com/capibytes/datalytics/domain/services/ConsultantService.java
@@ -1,7 +1,6 @@
 package com.capibytes.datalytics.domain.services;
 
 import com.capibytes.datalytics.domain.entities.Consultant;
-import com.capibytes.datalytics.dto.ConsultantResponseDto;
 import com.capibytes.datalytics.repositories.ConsultantRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class ConsultantService {
 
     @Autowired
