88dd1854181967e3a61afec1be41395d1e7274ad
Cecília Gonçalves <ceciliavgonc12@gmail.com>
2023-10-29 20:38:32 -0300

    Limpando repositório para fazer só o deploy

--- src/main/java/com/capibytes/datalytics/repositories/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capibytes.datalytics.repositories;
-
-import com.capibytes.datalytics.domain.entities.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository <Contact, Long>{
-}
--- terraform/ecr.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "cpb_backend_repo" {
-  name                 = "capibytes-backend-repo"
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
